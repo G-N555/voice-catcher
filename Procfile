3ed4616e63604754db655e437e364f93ff0e0963
Go Nakano <g.nakano555@gmail.com>
2019-08-29 11:57:20 +0900

    change name

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn editdojo_project.wsgi --log-file -
+web: gunicorn voice-picker.wsgi --log-file -
--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'editdojo_project.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'voice-picker.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
