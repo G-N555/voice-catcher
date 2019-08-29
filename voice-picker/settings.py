bbe39ce7fd229a0ac56637582ae19bf82e26e20c
Go Nakano <g.nakano555@gmail.com>
2019-08-29 18:26:25 +0900

    midified5

--- voice-picker/settings.py
+++ voice-picker/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0:$PORT', 'localhost',
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost',
                  'https://voice-picker-project.herokuapp.com']
 
 
